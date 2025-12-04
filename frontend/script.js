21698ebff8d622ecf5182e893f8e1c894eda99be
ronak <tanejaronak6@gmail.com>
2025-12-04 16:21:46 +0530

    Change fetch URL to use deployed backend

--- frontend/script.js
+++ frontend/script.js
@@ -118,7 +118,7 @@ async function sendMessage() {
 
     try {
         // We now fetch from our OWN Python Backend, not Groq directly
-        const response = await fetch('http://127.0.0.1:5000/chat', {
+        const response = await fetch('https://the-searchbot-1.onrender.com/chat', {
             method: 'POST',
             headers: { 'Content-Type': 'application/json' },
             body: JSON.stringify({ message: input })
